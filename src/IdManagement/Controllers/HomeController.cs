c27b045f3a0e144cc3311dcc917dd406037286d6
James Mckenna <G.James.Mckenna@hotmail.com>
2020-11-14 14:33:28 -0700

    Added a way to inspect cookies while debugging

--- src/IdManagement/Controllers/HomeController.cs
+++ src/IdManagement/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace IdManagement.Controllers
 {
@@ -18,20 +19,45 @@ namespace IdManagement.Controllers
         {
             _configuration = configuration;
         }
-        
+
+        //TODO: add logger if keeping this
+        private async Task<string> GetRefreshToken()
+        {
+            string refreshToken = await HttpContext.GetTokenAsync("refresh_token");
+            if (String.IsNullOrEmpty(refreshToken))
+            {
+                throw new NullReferenceException("No Access Token found");
+            }
+
+            return refreshToken;
+        }
+
+
+
         [AllowAnonymous]
         public IActionResult Index()
         {
-            ViewData["User"] = (HttpContext.User.Identity.IsAuthenticated) 
-                ? "Hello: " + HttpContext.User.Claims.First(c => c.Type == "name").Value 
-                : "You're not signed in?";
+            //ViewData["User"] = (HttpContext.User.Identity.IsAuthenticated) 
+            //    ? "Hello: " + HttpContext.User.Claims.First(c => c.Type == "name").Value 
+            //    : "You're not signed in?";
 
-            //Just to read cookies.....delete this at some point or add a controller action and page to render cookie values
+            //The Identity Model package for Identity Server 4 doesn't have silent renew functionality.
+            //I can get all tokens including the refresh token. I can request a new authentication session with the refresh token, 
+            //or the User can navigate to a new page and the session should be refreshed automatically. 
+            //I need to find/code a way to refresh the User session without doing a hard refresh/page navigation or implement session state.
+            //Save any form fields/User input to session state, prompt User to do a hard refresh and then reload any User input from session state. 
+
             var authCookie = HttpContext.Request.Cookies["IS4SDAC"];
 
+            var refreshToken = "";
+            if (HttpContext.User != null)
+            {
+                refreshToken = GetRefreshToken().Result;
+            }
+                     
             if (authCookie != null)
             {
-                var protectionProvider = DataProtectionProvider.Create(new DirectoryInfo(@"C:\Secrets\"),
+                var protectionProvider = DataProtectionProvider.Create(new DirectoryInfo(_configuration["SECRETS_DIR"]),
                         options =>
                         {
                             options.SetApplicationName(_configuration["Properties:ApplicationName"]);
@@ -49,6 +75,11 @@ namespace IdManagement.Controllers
                 //Get teh decrypted cookies as a Authentication Ticket
                 TicketDataFormat ticketDataFormat = new TicketDataFormat(protector);
                 AuthenticationTicket ticket = ticketDataFormat.Unprotect(authCookie);
+
+                var expires  = ticket.Properties.ExpiresUtc;
+                var now = DateTime.Now;
+                TimeSpan offSet = expires.Value - now;
+                //TempData["session"] = Math.Round(offSet.TotalSeconds, 0);
             }
 
             return View();
