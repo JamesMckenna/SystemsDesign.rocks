aac84ab5ca2a769eef266f96d41c65b8623ca6f0
James Mckenna <G.James.Mckenna@hotmail.com>
2020-11-14 13:58:12 -0700

    Changed persistence from SQLite to SQL Server

---
+++ IdentityDataCommon/ApplicationDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace IdentityDataCommon
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            //this doesn't work if the code is not run - EG: Dotnet run. A developer uses a class library, but doesn't run it.
+            //string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"); //Get environment doesn't work even if there is a launchSettings.json in the library. 
+            //string secretsFile = Environment.GetEnvironmentVariable("SECRETS_PATH"); //Get environment doesn't work even if there is a launchSettings.json in the library.
+
+            //Build config
+            IConfiguration _configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                //Since this is a designtime operation, a hardcoded value to perform a EntityFramework Migration is not the end of the world.
+                .AddJsonFile("C:\\Secrets\\AppSecrets.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer(_configuration["IdApiConnectionStrings:IdentityDB"]);
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
--- IdentityDataCommon/Migrations/20201110181355_InitialSqlServer.Designer.cs
+++ IdentityDataCommon/Migrations/20201110181355_InitialSqlServer.Designer.cs
@@ -4,14 +4,16 @@ using IdentityDataCommon;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IdentityDataCommon.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201110181355_InitialSqlServer")]
+    partial class InitialSqlServer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
