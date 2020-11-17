3f725695c449e158d7697445f0953ccd154741ba
James Mckenna <G.James.Mckenna@hotmail.com>
2020-11-17 14:15:35 -0700

    Experimented with logging

--- src/IS4/Program.cs
+++ src/IS4/Program.cs
@@ -7,11 +7,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Events;
 using Serilog.Extensions.Logging;
 using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 
 namespace IS4
 {
@@ -21,31 +23,32 @@ namespace IS4
         public static int Main(string[] args)
         {
             Activity.DefaultIdFormat = ActivityIdFormat.W3C;
-
-            //Log.Logger = new LoggerConfiguration()
-            //.MinimumLevel.Debug()
-            //.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-            //.MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
-            //.MinimumLevel.Override("Microsoft.AspNetCore.Diagnostics.ExceptionHandlerMiddleware", LogEventLevel.Fatal)
-            //.Enrich.FromLogContext()
-            //.Filter.ByExcluding("RequestPath = '/lib/*' and StatusCode = 200")
-            //.Filter.ByExcluding("RequestPath = '/js' and StatusCode = 200")
-            //.Filter.ByExcluding("RequestPath = '/css' and StatusCode = 200")
-            //.WriteTo.Console()
-            //.WriteTo.Providers(Providers)
-            //.WriteTo.File("..\\Logs\\IS4\\IS4.log", LogEventLevel.Information,
-            //            outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
-            //            null, 1073741824, null, true, false, TimeSpan.FromSeconds(1), RollingInterval.Day, true, 31, null, null)
-            //.CreateLogger();
+            var name = Assembly.GetExecutingAssembly().GetName();
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.AspNetCore.Diagnostics.ExceptionHandlerMiddleware", LogEventLevel.Fatal)
+            .Enrich.FromLogContext()
+            .Enrich.WithProperty("Assembly", $"{name.Name}")
+            .Filter.ByExcluding("RequestPath = '/lib/*' or StatusCode = 200")
+            .Filter.ByExcluding("RequestPath = '/js' or StatusCode = 200")
+            .Filter.ByExcluding("RequestPath = '/css' or StatusCode = 200")
+            .WriteTo.Console()
+            .WriteTo.Providers(Providers)
+            .WriteTo.File(".\\Logs\\IS4.log", LogEventLevel.Information,
+                        outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {Level:u3}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",
+                        null, 1073741824, null, true, false, TimeSpan.FromSeconds(1), RollingInterval.Day, true, 31, null, null)
+            .CreateLogger();
 
             var configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile( "appsettings.json", false, true)
                 .Build();
 
-            Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(configuration)
-                .CreateLogger();
+            //Log.Logger = new LoggerConfiguration()
+            //    .ReadFrom.Configuration(configuration)
+            //    .CreateLogger();
 
             Log.Information("Logging from IS4 Program.cs - IS4 starting up");
 
--- src/IdManagement/Program.cs
+++ src/IdManagement/Program.cs
@@ -3,11 +3,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Events;
 using Serilog.Extensions.Logging;
 using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 
 namespace IdManagement
 {
@@ -17,32 +19,32 @@ namespace IdManagement
         public static int Main(string[] args)
         {
             Activity.DefaultIdFormat = ActivityIdFormat.W3C;
-            //var name = Assembly.GetExecutingAssembly().GetName();
-            //Log.Logger = new LoggerConfiguration()
-            //.MinimumLevel.Debug()
-            //.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-            //.MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
-            //.MinimumLevel.Override("Microsoft.AspNetCore.Diagnostics.ExceptionHandlerMiddleware", LogEventLevel.Fatal)//Prevents multiple log entries for the same error
-            //.Enrich.FromLogContext()
-            //.Enrich.WithProperty("Assembly", $"{name.Name}")
-            //.Filter.ByExcluding("RequestPath = '/lib/*' and StatusCode = 200")
-            //.Filter.ByExcluding("RequestPath = '/js' and StatusCode = 200")
-            //.Filter.ByExcluding("RequestPath = '/css' and StatusCode = 200")
-            //.WriteTo.Console()
-            //.WriteTo.Providers(Providers)
-            //.WriteTo.File("..\\Logs\\IdManagement\\IdManagement.log", LogEventLevel.Information, 
-            //            outputTemplate: "\n{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Properties}{NewLine}{Exception}", 
-            //            null, 1073741824, null, true, false, TimeSpan.FromSeconds(1), RollingInterval.Day, true, 31, null, null)
-            //.CreateLogger();
+            var name = Assembly.GetExecutingAssembly().GetName();
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.AspNetCore.Diagnostics.ExceptionHandlerMiddleware", LogEventLevel.Fatal)//Prevents multiple log entries for the same error
+            .Enrich.FromLogContext()
+            .Enrich.WithProperty("Assembly", $"{name.Name}")
+            .Filter.ByExcluding("RequestPath = '/lib/*' and StatusCode = 200")
+            .Filter.ByExcluding("RequestPath = '/js' and StatusCode = 200")
+            .Filter.ByExcluding("RequestPath = '/css' and StatusCode = 200")
+            .WriteTo.Console()
+            .WriteTo.Providers(Providers)
+            .WriteTo.File(".\\Logs\\IdManagement.log", LogEventLevel.Information,
+                        outputTemplate: "\n[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {Level:u3}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Properties}{NewLine}{Exception}{NewLine}",
+                        null, 1073741824, null, true, false, TimeSpan.FromSeconds(1), RollingInterval.Day, true, 31, null, null)
+            .CreateLogger();
 
             var configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", false, true)
                 .Build();
 
-            Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(configuration)
-                .CreateLogger();
+            //Log.Logger = new LoggerConfiguration()
+            //    .ReadFrom.Configuration(configuration)
+            //    .CreateLogger();
 
             Log.Information("Logging from Id Management Program.cs - Id Management starting up");
 
