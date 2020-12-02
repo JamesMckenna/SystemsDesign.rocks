beddf9908303fbac61b01f3bc40c0d21ea16e365
James Mckenna <G.James.Mckenna@hotmail.com>
2020-12-02 15:03:11 -0700

    Added regex annotation, fixed error message

--- src/IdManagement/Models/AccountViewModels/RegisterViewModel.cs
+++ src/IdManagement/Models/AccountViewModels/RegisterViewModel.cs
@@ -5,7 +5,8 @@ namespace IdManagement.Models.AccountViewModels
     public class RegisterViewModel
     {
         [Required]
-        [StringLength(25, ErrorMessage = "User Name must be unique and at least {2} characters long.", MinimumLength = 4)]
+        [RegularExpression(@"^[a-zA-Z0-9-._+]{4,25}$", ErrorMessage = "Upper or lowercase English letters a - z, numbers 0 - 9, and - . _ + are valid characters for User Name")]
+        [StringLength(25, ErrorMessage = "User Name must be unique and at least {0} characters long.", MinimumLength = 4)]
         [Display(Name = "User Name")]
         public string UserName { get; set; }
 
@@ -15,7 +16,7 @@ namespace IdManagement.Models.AccountViewModels
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The password must be at least {0} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
